15ddd0b8fbd96d8f4fbf91841905bff1fc5cb0c4
Rick <Riockmert@gmail.com>
2016-11-05 20:49:20 +0100

    init

---
+++ Mazes/Classes/Cell.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Mazes.Classes
+{
+    public enum Directions
+    {
+        N = 0,
+        E = 1,
+        S = 2,
+        W = 3
+    }
+
+    public class Cell
+    {
+        public Point Location { get; set; }
+        public int Size { get; set; }
+        public List<Directions> Directions {get; set;}
+
+        public bool IsEmpty
+        {
+            get
+            {
+                if (this.Directions.Count == 0 || this.Directions == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public Cell(Point location, int size)
+        {
+            this.Location = location;
+            this.Size = size;
+        }    
+    }
+}
---
+++ Mazes/Classes/Grid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mazes.Classes
+{
+    public class Grid
+    {
+        public Cell[,] Cells { get; set; }
+
+        public Grid(int width, int height, int cellsize)
+        {
+            Cells = new Cell[height / cellsize, width / cellsize];
+        }
+    }
+}
